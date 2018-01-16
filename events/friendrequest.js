4910d29009a91f67ce3c089432c69c1b383345ff
FHGDev <gideon.lingle14@gmail.com>
2018-01-16 09:50:22 -0600

    Update friendrequest.js

--- events/friendrequest.js
+++ events/friendrequest.js
@@ -1,9 +1,9 @@
 const roblox = require('roblox.js')
 
-module.exports = (id, bot, args) => {
+module.exports = (id, bot) => {
   let u = bot.users.find('id', '294194506113220608')
   let name = roblox.getUsernameFromId(id)
-  let a = 294194506113220608
+  let a = 105604974
   let au = roblox.getUsernameFromId(a)
   u.send(`${au}, you have recieved a friend request from ${name} on roblox!)
 }
