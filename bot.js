ad9dc135b9ef5b5e1ae033c6a4f1c91a74a50f73
FHGDev <gideon.lingle14@gmail.com>
2018-01-22 14:31:29 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,20 @@ const discord = require('discord.js');
 const bot = new discord.Client();
 const prefix = "!";
 const maxrank = 210;
-const groupId = 3152407;
+const groupId = 127081;
 const roblox = require('roblox-js');
 bot.login(process.env.tok);
+
+bot.commands = new discord.Collection
+
+require('fs').readdir("./commands/", (err, files) => {
+	console.log("Loading Commands...");
+	if (err) return console.error;
+	files.filter(f => f.split(".").pop() == "js").forEach((f, i) => {
+		bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+	})
+});
+
 roblox.login({username: "FreakingHulk", password: process.env.password}).then((success) => {
   console.log(`Successfully Logged in with Username FreakingHulk, and password ${process.env.password}`)
 }).catch(() => {console.log("Failed to login.");});
@@ -22,36 +33,11 @@ function isCommand(command, message){
 
 bot.on('message', (message) => {
 	if (message.author.bot) return; // Dont answer yourself.
-    var args = message.content.split(/[ ]+/)
-    
-    if(isCommand('Promote', message)){
-    	var username = args[1]
-    	if (username){
-    		message.channel.send(`Checking ROBLOX for ${username}`)
-    		roblox.getIdFromUsername(username)
-			.then(function(id){
-				roblox.getRankInGroup(groupId, id)
-				.then(function(rank){
-					if(maximumRank <= rank){
-						message.channel.send(`${id} is rank ${rank} and not promotable.`)
-					} else {
-						message.channel.send(`${id} is rank ${rank} and promotable.`)
-						roblox.promote(groupId, id)
-						.then(function(roles){
-							message.channel.send(`Promoted from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
-						}).catch(function(err){
-							message.channel.send("Failed to promote.")
-						});
-					}
-				}).catch(function(err){
-					message.channel.send("Couldn't get him in the group.")
-				});
-			}).catch(function(err){ 
-				message.channel.send(`Sorry, but ${username} doesn't exist on ROBLOX.`)
-			});
-    	} else {
-    		message.channel.send("Please enter a username.")
-    	}
-    	return;
-    }
-});
+    var mArray = message.content.split(" ")
+	var args = mArray.slice(1)
+	let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+	if (cmd) {
+		cmd.run(bot, message, args, discord)
+		console.log(`${message.author.username} used the ${message.content.split(" ")[0]} command.`)
+	}
+})
