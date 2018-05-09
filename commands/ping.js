0de75a71a1ae433f966cfda6d9b58a8243bdde56
FHGDev <gideon.lingle14@gmail.com>
2018-05-08 20:44:37 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,11 @@
 module.exports.run = (bot, message, args, discord) => {
   let embed = new discord.RichEmbed()
   .setTitle(`ROBLOX Ping`)
-  .setDescription(`:ping_pong: PONG! My ping is ${bot.ping}`)
+  .setDescription(`:ping_pong: PONG! My ping is ${Math.floor(bot.ping)}ms`)
   .setColor(`BLUE`)
   .setFooter(`ROBLOX Ping Menu...`)
+  .setTimestamp()
+  message.channel.send({embed: embed})
 }
 
 module.exports.help = {
