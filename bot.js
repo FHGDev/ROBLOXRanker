a994979569bdf80e3750db5ddf624c4aad5f03a1
FHGDev <gideon.lingle14@gmail.com>
2018-05-08 20:41:21 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
 const prefix = "!";
-const maxrank = 210;
-const groupId = 127081;
+const maxrank = "210";
+const groupId = "3152407";
 const roblox = require('roblox-js');
 bot.login(process.env.tok);
 
@@ -18,7 +18,7 @@ require('fs').readdir("./commands/", (err, files) => {
 
 roblox.login({username: "FreakingHulk", password: process.env.password}).then((success) => {
   console.log(`Successfully Logged in with Username FreakingHulk, and password ${process.env.password}`)
-}).catch(() => console.log("Failed to login."););
+}).catch(() => console.log("Failed to login."));
 
 bot.on("ready", () => {
   bot.user.setActivity(`for !help | ${bot.guilds.array().length}`, {type: "WATCHING"})
